3ffbebc800d9856a6a2bb88229d809ba559585df
Lauren Capell <laurencapell@Lauren-Capells-iMac.local>
2012-08-09 19:09:21 -0400

    Added first files to the repo.

---
+++ main.js
@@ -0,0 +1,32 @@
+// Loops
+// Author: Lauren Capell
+
+// while-loop
+/*
+
+var bonab = 99; // iterator, counter
+while (bonab > 0) {
+	console.log(bonab + " bottles of non-alcoholic beverage on the wall.");
+	bonab--;
+};
+
+console.log("No more bottles of non-alcoholic beverage.");
+*/
+
+
+// for-loop
+
+for (var bonab = 99; bonab > 0; bonab--) {
+	console.log(bonab + " bottles of non-alcoholic beverage on the wall.");
+};
+
+console.log("No more bottles of non-alcoholic beverage.");
+
+
+
+for (var i = 0; i < max; i++) {
+	for (var ii = 0; i < 3; ii++) {
+		console.log( "i:" + i + ", ii:" + ii );
+	};
+};
+
